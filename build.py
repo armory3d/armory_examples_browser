9df696a06da3b5cb0ea0f8eb10727fd6977beea4
tong <tong@disktree.net>
2021-06-24 13:05:36 +0200

    Update build.py

--- build.py
+++ build.py
@@ -1,7 +1,6 @@
 import sys, os
 import bpy, arm
 
-#bpy.ops.arm.clean_project()
 wrd = bpy.data.worlds['Arm']
 if(len(wrd.arm_exporterlist)==0):
     wrd.arm_exporterlist.add()
@@ -9,12 +8,5 @@ if(len(wrd.arm_exporterlist)==0):
     wrd.arm_exporterlist[-1].name = 'Preset'
     wrd.arm_exporterlist[-1].arm_project_target = 'html5'
     wrd.arm_exporterlist[-1].arm_project_scene = bpy.context.scene
-# else:
-#     for e in wrd.arm_exporterlist:
-#         print(e)
-# print(len(wrd.arm_exporterlist))
-# print(wrd.arm_exporterlist[0])
-#bpy.data.worlds['Arm'].arm_recompile = True
-#assets.invalidate_compiled_data(self, context)
+bpy.ops.arm.clean_project()
 bpy.ops.arm.publish_project()
-exit
