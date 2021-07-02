cb7769fcd82f2eaaa74c4afa1f4c8b0e6b5a6fbf
tong <tong@disktree.net>
2021-07-02 15:16:43 +0200

    USe temp export target for every build

--- build.py
+++ build.py
@@ -1,15 +1,18 @@
 import sys, os
 import bpy, arm
+import arm.utils
+
+#target = sys.argv.pop()
+#mode = sys.argv.pop()
 
 wrd = bpy.data.worlds['Arm']
-if(len(wrd.arm_exporterlist)==0):
-    wrd.arm_exporterlist.add()
-    wrd.arm_exporterlist_index = len(wrd.arm_exporterlist) - 1
-    wrd.arm_exporterlist[-1].name = 'Preset'
-    wrd.arm_exporterlist[-1].arm_project_target = 'html5'
-    wrd.arm_exporterlist[-1].arm_project_scene = bpy.context.scene
-# else:
-#     print( wrd.arm_exporterlist[0])
+# if(len(wrd.arm_exporterlist)==0):
+wrd.arm_exporterlist.add()
+wrd.arm_exporterlist_index = len(wrd.arm_exporterlist) - 1
+wrd.arm_exporterlist[wrd.arm_exporterlist_index].name = 'Temp'
+wrd.arm_exporterlist[wrd.arm_exporterlist_index].arm_project_target = 'html5'
+wrd.arm_exporterlist[wrd.arm_exporterlist_index].arm_project_scene = bpy.context.scene
 
 # bpy.ops.arm.clean_project()
+# bpy.ops.arm.build_project()
 bpy.ops.arm.publish_project()
