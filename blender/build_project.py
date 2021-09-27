89deb3e5d749805ae44529a7ebdd4c4e7b925ee2
tong <tong@disktree.net>
2021-09-27 19:16:36 +0200

    Cleanup

--- blender/build_project.py
+++ blender/build_project.py
@@ -1,20 +1,28 @@
 import sys, os
-import bpy, arm
-import arm.utils
+import bpy
+import arm, arm.utils
+import arm.make_state as state
 
+# exporter = sys.argv[0]
+# print(exporter)
+target = 'html5'
 #target = sys.argv.pop()
 #mode = sys.argv.pop()
 
 wrd = bpy.data.worlds['Arm']
 # if(len(wrd.arm_exporterlist)==0):
+
+# wrd.arm_verbose_output=True
+
 wrd.arm_exporterlist.add()
-wrd.arm_exporterlist_index = len(wrd.arm_exporterlist) - 1
 wrd.arm_exporterlist[wrd.arm_exporterlist_index].name = 'Temp'
-wrd.arm_exporterlist[wrd.arm_exporterlist_index].arm_project_target = 'html5'
+wrd.arm_exporterlist[wrd.arm_exporterlist_index].arm_project_target = target
 wrd.arm_exporterlist[wrd.arm_exporterlist_index].arm_project_scene = bpy.context.scene
+wrd.arm_exporterlist_index = len(wrd.arm_exporterlist) - 1
 
 # bpy.ops.arm.clean_project()
 # bpy.ops.arm.build_project()
 bpy.ops.arm.publish_project()
-
-bpy.ops.wm.quit_blender()
+# bpy.ops.wm.quit_blender()
+code = state.proc_build.poll()
+sys.exit(code)
