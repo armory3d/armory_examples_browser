26ad0e586df3bac8ea0215a43a6e8aa68609f82f
tong <tong@disktree.net>
2021-09-27 19:33:05 +0200

    Add build

--- blender/enable_addon.py
+++
@@ -1,9 +0,0 @@
-import os
-import bpy, addon_utils
-
-state = addon_utils.check("armory")
-if not state[1]:
-    bpy.ops.preferences.addon_install(filepath='armsdk/armory.py')
-    bpy.ops.preferences.addon_enable(module='armory')
-    bpy.context.preferences.addons["armory"].preferences['sdk_path'] = os.getcwd()+'/armsdk/'
-    bpy.ops.wm.save_userpref()
---
+++ examples/input_mouselock/README.md
@@ -0,0 +1,3 @@
+Keyboard `Escape` to toggle mouse lock.
+
+https://armory3d.org/api/iron/system/Mouse.html
